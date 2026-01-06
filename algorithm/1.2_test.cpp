3f366a4e5ac719dedb05757551b36ceeb2ab3a4e
ffuqiangfuu <fuqiang_liu2003@163.com>
2026-01-06 09:47:46 +0800

    1

--- algorithm/1.2_test.cpp
+++ algorithm/1.2_test.cpp
@@ -1,22 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int joyvan[1001][1001];
-int row[1001];
-int col[1001];
-
+int matrix[1001][1001];
+int row[1001],col[1001];
+int n,m,q;
 int main(){
-    int n,m,q;
     cin>>n>>m>>q;
-    for(int i=1;i<=n;i++){              //从1开始，交换第一行和第二行，就是对应数组的第一行第二行
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            scanf("%d",&joyvan[i][j]);
+            scanf("%d",&matrix[i][j]);
         }
     }
-    for(int i=1;i<=1000;i++){           //row[i]的意思就是表面上第i行，实际上存的是第几行
+    for(int i=1;i<=1000;i++){
         row[i]=i;
         col[i]=i;
     }
+
     while(q--){
         int op,x,y;
         scanf("%d %d %d",&op,&x,&y);
@@ -25,9 +24,8 @@ int main(){
         }else if(op==1){
             swap(col[x],col[y]);
         }else{
-            printf("%d\n",joyvan[row[x]][col[y]]);  //注意这个\n别漏了
+            printf("%d\n",matrix[row[x]][col[y]]);
         }
     }
-
     return 0;
 }
